a3e261ffd650f4bb6e8137f5283885f98cf3f2bb
Péter Pandula <ppeter@halcyonmobile.com>
2017-12-21 11:20:15 +0200

    Improve item action button aligment.

--- app/src/main/java/com/pandulapeter/campfire/feature/home/shared/SpacesItemDecoration.kt
+++ app/src/main/java/com/pandulapeter/campfire/feature/home/shared/SpacesItemDecoration.kt
@@ -13,7 +13,7 @@ class SpacesItemDecoration(private var space: Int) : RecyclerView.ItemDecoration
         outRect.apply {
             left = space
             top = if (parent.getChildAdapterPosition(view) == 0) space else 0
-            right = space
+            right = 0
             bottom = space
         }
     }
