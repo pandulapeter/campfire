992ce176bdc4d8bfa6bed590420aef9602aade4e
Péter Pandula <ppeter@halcyonmobile.com>
2017-12-24 11:10:55 +0200

    Rewrite AppShortcutManager.

--- app/src/main/java/com/pandulapeter/campfire/integration/AppShortcutManager.kt
+++ app/src/main/java/com/pandulapeter/campfire/integration/AppShortcutManager.kt
@@ -5,7 +5,6 @@ import android.content.Intent
 import android.content.pm.ShortcutInfo
 import android.content.pm.ShortcutManager
 import android.graphics.drawable.Icon
-import android.net.Uri
 import android.os.Build
 import android.support.annotation.DrawableRes
 import android.support.annotation.RequiresApi
@@ -20,27 +19,45 @@ import kotlin.math.min
  *
  * TODO: Does not work as expected.
  */
-class AppShortcutManager(private val context: Context,
-                         private val dataStorageManager: DataStorageManager,
-                         private val playlistRepository: PlaylistRepository) {
+class AppShortcutManager(context: Context, dataStorageManager: DataStorageManager, playlistRepository: PlaylistRepository) : Features {
+    private val implementation = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) Implementation(context, dataStorageManager, playlistRepository) else CompatImplementation()
 
-    fun onPlaylistOpened(playlistId: Int) {
-        val list = dataStorageManager.playlistHistory.toMutableList().apply { add(0, playlistId.toString()) }.distinct()
-        dataStorageManager.playlistHistory = list.subList(0, min(list.size, 3))
-        updateAppShortcuts()
-    }
+    override fun onLibraryOpened() = implementation.onLibraryOpened()
 
-    fun updateAppShortcuts() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N_MR1) {
+    override fun onCollectionsOpened() = implementation.onCollectionsOpened()
+
+    override fun onPlaylistOpened(playlistId: Int) = implementation.onPlaylistOpened(playlistId)
+
+    override fun updateAppShortcuts() = implementation.updateAppShortcuts()
+
+    @RequiresApi(Build.VERSION_CODES.N_MR1)
+    private class Implementation(private val context: Context,
+                                 private val dataStorageManager: DataStorageManager,
+                                 private val playlistRepository: PlaylistRepository) : Features {
+        private val shortcutManager: ShortcutManager = context.getSystemService(Context.SHORTCUT_SERVICE) as ShortcutManager
+
+        override fun onLibraryOpened() = trackAppShortcutUsage(LIBRARY_ID)
+
+        override fun onCollectionsOpened() = trackAppShortcutUsage(COLLECTIONS_ID)
+
+        override fun onPlaylistOpened(playlistId: Int) {
+            trackAppShortcutUsage(playlistId.toString())
+            val list = dataStorageManager.playlistHistory.toMutableList().apply { add(0, playlistId.toString()) }.distinct()
+            dataStorageManager.playlistHistory = list.subList(0, min(list.size, 3))
+            updateAppShortcuts()
+        }
+
+        override fun updateAppShortcuts() {
+            removeAppShortcuts()
             val shortcuts = mutableListOf<ShortcutInfo>()
             shortcuts.add(createAppShortcut(
-                "library",
+                LIBRARY_ID,
                 context.getString(R.string.home_library),
                 context.getString(R.string.app_shortcut_open_library),
                 R.drawable.ic_library_24dp,
                 HomeViewModel.NavigationItem.Library))
             shortcuts.add(createAppShortcut(
-                "collections",
+                COLLECTIONS_ID,
                 context.getString(R.string.home_collections),
                 context.getString(R.string.app_shortcut_open_collections),
                 R.drawable.ic_collections_24dp,
@@ -49,26 +66,57 @@ class AppShortcutManager(private val context: Context,
                 playlistRepository.getPlaylist(it.toInt())?.let { playlist ->
                     val title = playlist.title ?: context.getString(R.string.home_favorites)
                     shortcuts.add(createAppShortcut(
-                        playlist.id.toString(),
+                        PLAYLIST_ID + playlist.id,
                         title,
                         context.getString(R.string.app_shortcut_open_playlist, title),
                         R.drawable.ic_playlist_24dp,
                         HomeViewModel.NavigationItem.Playlist(playlist.id)))
                 }
             }
-            (context.getSystemService(Context.SHORTCUT_SERVICE) as ShortcutManager).dynamicShortcuts = shortcuts
+            shortcutManager.dynamicShortcuts = shortcuts
+        }
+
+        private fun createAppShortcut(id: String,
+                                      shortLabel: String,
+                                      longLabel: String,
+                                      @DrawableRes icon: Int,
+                                      navigationItem: HomeViewModel.NavigationItem) = ShortcutInfo.Builder(context, id)
+            .setShortLabel(shortLabel)
+            .setLongLabel(longLabel)
+            .setIcon(Icon.createWithResource(context, icon)) //TODO: Tint icon.
+            .setIntent(Intent().setAction(Intent.ACTION_VIEW)) //TODO: Handle navigationItem.
+            .build()
+
+        private fun removeAppShortcuts() = shortcutManager.removeAllDynamicShortcuts()
+
+        private fun trackAppShortcutUsage(id: String) = shortcutManager.reportShortcutUsed(id)
+
+        private companion object {
+            const val LIBRARY_ID = "library"
+            const val COLLECTIONS_ID = "collections"
+            const val PLAYLIST_ID = "playlist_"
         }
     }
 
-    @RequiresApi(Build.VERSION_CODES.N_MR1)
-    private fun createAppShortcut(id: String,
-                                  shortLabel: String,
-                                  longLabel: String,
-                                  @DrawableRes icon: Int,
-                                  navigationItem: HomeViewModel.NavigationItem) = ShortcutInfo.Builder(context, id)
-        .setShortLabel(shortLabel)
-        .setLongLabel(longLabel)
-        .setIcon(Icon.createWithResource(context, icon)) //TODO: Tint icon.
-        .setIntent(Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/"))) //TODO: Do something with navigationItem.
-        .build()
+    private class CompatImplementation : Features {
+
+        override fun onLibraryOpened() = Unit
+
+        override fun onCollectionsOpened() = Unit
+
+        override fun onPlaylistOpened(playlistId: Int) = Unit
+
+        override fun updateAppShortcuts() = Unit
+    }
+}
+
+private interface Features {
+
+    fun onLibraryOpened()
+
+    fun onCollectionsOpened()
+
+    fun onPlaylistOpened(playlistId: Int)
+
+    fun updateAppShortcuts()
 }
