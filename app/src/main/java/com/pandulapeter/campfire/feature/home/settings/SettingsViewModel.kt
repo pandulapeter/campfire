b807f3c15a45d6e4a8b1d66833b37eb74c188025
Pandula Péter <pandulapeter@gmail.com>
2018-01-23 18:05:46 +0200

    Auto-generate chord name examples.

--- app/src/main/java/com/pandulapeter/campfire/feature/home/settings/SettingsViewModel.kt
+++ app/src/main/java/com/pandulapeter/campfire/feature/home/settings/SettingsViewModel.kt
@@ -1,10 +1,12 @@
 package com.pandulapeter.campfire.feature.home.settings
 
 import android.databinding.ObservableBoolean
+import android.databinding.ObservableField
 import com.pandulapeter.campfire.data.repository.UserPreferenceRepository
 import com.pandulapeter.campfire.feature.home.shared.homeChild.HomeChildViewModel
 import com.pandulapeter.campfire.networking.AnalyticsManager
 import com.pandulapeter.campfire.util.onPropertyChanged
+import com.pandulapeter.campfire.util.toNoteName
 
 /**
  * Handles events and logic for [SettingsFragment].
@@ -16,9 +18,22 @@ class SettingsViewModel(
     val shouldAllowToolbarScrolling = ObservableBoolean()
     val shouldShowChords = ObservableBoolean(userPreferenceRepository.shouldShowChords)
     val shouldUseGermanNotation = ObservableBoolean(userPreferenceRepository.shouldUseGermanNotation)
+    val shouldUseGermanNotationDescription = ObservableField(generateNotationExample(shouldUseGermanNotation.get()))
 
     init {
         shouldShowChords.onPropertyChanged { userPreferenceRepository.shouldShowChords = it }
-        shouldUseGermanNotation.onPropertyChanged { userPreferenceRepository.shouldUseGermanNotation = it }
+        shouldUseGermanNotation.onPropertyChanged {
+            userPreferenceRepository.shouldUseGermanNotation = it
+            shouldUseGermanNotationDescription.set(generateNotationExample(it))
+        }
     }
+
+    private fun generateNotationExample(shouldUseGermanNotation: Boolean) =
+        0.toNoteName(shouldUseGermanNotation) + ", " +
+                2.toNoteName(shouldUseGermanNotation) + ", " +
+                4.toNoteName(shouldUseGermanNotation) + ", " +
+                5.toNoteName(shouldUseGermanNotation) + ", " +
+                7.toNoteName(shouldUseGermanNotation) + ", " +
+                9.toNoteName(shouldUseGermanNotation) + ", " +
+                11.toNoteName(shouldUseGermanNotation)
 }
