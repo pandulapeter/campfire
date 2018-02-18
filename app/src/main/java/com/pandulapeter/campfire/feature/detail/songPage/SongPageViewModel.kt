5537982f30d8f4623c75537c13e7c3e1c6b3780f
Pandula Péter <pandulapeter@gmail.com>
2018-02-18 20:22:32 +0200

    Improve navigation drawer selection color.

--- app/src/main/java/com/pandulapeter/campfire/feature/detail/songPage/SongPageViewModel.kt
+++ app/src/main/java/com/pandulapeter/campfire/feature/detail/songPage/SongPageViewModel.kt
@@ -50,7 +50,6 @@ class SongPageViewModel(
             if (modifiedValue != it) {
                 transposition.set(modifiedValue)
             }
-            //TODO: Buggy.
             refreshText()
         }
     }
