66a6a6585e661b87ac18b20fec0d934592c7b0f1
Pandula Péter <pandulapeter@gmail.com>
2018-02-20 20:18:02 +0200

    Fix EllipsizeSpan sometimes adding unnecessary linebreaks.

--- app/src/main/java/com/pandulapeter/campfire/feature/shared/EllipsizeLineSpan.kt
+++ app/src/main/java/com/pandulapeter/campfire/feature/shared/EllipsizeLineSpan.kt
@@ -4,9 +4,9 @@ import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.Rect
 import android.support.annotation.ColorInt
-import android.support.annotation.ColorRes
 import android.text.style.LineBackgroundSpan
 import android.text.style.ReplacementSpan
+import kotlin.math.ceil
 
 /**
  * Ellipsizes individual lines of a text.
@@ -15,38 +15,34 @@ class EllipsizeLineSpan(@ColorInt private val color: Int? = null) : ReplacementS
     private var layoutLeft = 0
     private var layoutRight = 0
 
-    override fun drawBackground(
-        canvas: Canvas,
-        paint: Paint,
-        left: Int,
-        right: Int,
-        top: Int,
-        baseline: Int,
-        bottom: Int,
-        text: CharSequence,
-        start: Int,
-        end: Int,
-        lnum: Int
-    ) {
+    override fun drawBackground(canvas: Canvas, paint: Paint, left: Int, right: Int, top: Int, baseline: Int, bottom: Int, text: CharSequence, start: Int, end: Int, lnum: Int) {
         val clipRect = Rect()
         canvas.getClipBounds(clipRect)
         layoutLeft = clipRect.left
         layoutRight = clipRect.right
     }
 
-    override fun getSize(paint: Paint, text: CharSequence, start: Int, end: Int, fm: Paint.FontMetricsInt?) = layoutRight - layoutLeft
+    override fun getSize(paint: Paint, text: CharSequence, start: Int, end: Int, fontMetricsInt: Paint.FontMetricsInt?): Int {
+        fontMetricsInt?.let {
+            it.top = paint.getFontMetricsInt(it)
+        }
+        return Math.round(paint.measureText(text, start, start))
+    }
 
     override fun draw(canvas: Canvas, text: CharSequence, start: Int, end: Int, x: Float, top: Int, y: Int, bottom: Int, paint: Paint) {
         color?.let { paint.color = it }
-        var newEnd = end
-        val textWidth = paint.measureText(text, start, newEnd)
-        if (x + Math.ceil(textWidth.toDouble()).toInt() < layoutRight) {
-            canvas.drawText(text, start, newEnd, x, y.toFloat(), paint)
+        val textWidth = paint.measureText(text, start, end)
+        if (x + ceil(textWidth) < layoutRight) {
+            canvas.drawText(text, start, end, x, y.toFloat(), paint)
         } else {
-            val ellipsizeWidth = paint.measureText("\u2026")
-            newEnd = start + paint.breakText(text, start, newEnd, true, layoutRight.toFloat() - x - ellipsizeWidth, null)
+            val ellipsizeWidth = paint.measureText(ELLIPSIZE_CHARACTER)
+            val newEnd = start + paint.breakText(text, start, end, true, layoutRight - x - ellipsizeWidth, null)
             canvas.drawText(text, start, newEnd, x, y.toFloat(), paint)
-            canvas.drawText("\u2026", x + paint.measureText(text, start, newEnd), y.toFloat(), paint)
+            canvas.drawText(ELLIPSIZE_CHARACTER, x + paint.measureText(text, start, newEnd), y.toFloat(), paint)
         }
+    }
+
+    companion object {
+        private const val ELLIPSIZE_CHARACTER = "\u2026"
     }
 }
