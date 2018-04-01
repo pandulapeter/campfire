be330e5afa0cf5e86f0241b5f31ed48dead3b10c
Péter Pandula <peter@halcyonmobile.com>
2018-04-01 16:14:55 +0300

    Implement local caching.

--- app/src/main/java/com/pandulapeter/campfire/data/dao/SongDao.kt
+++ app/src/main/java/com/pandulapeter/campfire/data/dao/SongDao.kt
@@ -2,8 +2,8 @@ package com.pandulapeter.campfire.data.dao
 
 import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Insert
-import android.arch.persistence.room.OnConflictStrategy.REPLACE
 import android.arch.persistence.room.Query
+import android.arch.persistence.room.Transaction
 import com.pandulapeter.campfire.data.model.Song
 
 @Dao
@@ -12,8 +12,14 @@ interface SongDao {
     @Query("SELECT * from ${Song.TABLE_NAME}")
     fun getAll(): List<Song>
 
-    @Insert(onConflict = REPLACE)
-    fun insert(song: Song)
+    @Transaction
+    fun updateData(songs: List<Song>) {
+        deleteAll()
+        insertAll(songs)
+    }
+
+    @Insert
+    abstract fun insertAll(songs: List<Song>)
 
     @Query("DELETE from ${Song.TABLE_NAME}")
     fun deleteAll()
--- app/src/main/java/com/pandulapeter/campfire/data/repository/SongRepository.kt
+++ app/src/main/java/com/pandulapeter/campfire/data/repository/SongRepository.kt
@@ -5,10 +5,25 @@ import com.pandulapeter.campfire.data.model.Song
 import com.pandulapeter.campfire.data.repository.shared.Repository
 import com.pandulapeter.campfire.networking.NetworkManager
 import com.pandulapeter.campfire.util.enqueueCall
+import kotlinx.coroutines.experimental.CommonPool
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
 
 class SongRepository(private val networkManager: NetworkManager, private val songDatabase: SongDatabase) : Repository<List<Song>>() {
 
     override val data = mutableListOf<Song>()
+
+    init {
+        async(UI) {
+            async(CommonPool) {
+                songDatabase.songDao().getAll()
+            }.await().let {
+                data.clear()
+                data.addAll(it)
+                notifySubscribers()
+            }
+        }
+    }
 
     fun isDataAvailable() = data.isNotEmpty()
 
@@ -18,9 +33,8 @@ class SongRepository(private val networkManager: NetworkManager, private val son
                 data.clear()
                 data.addAll(it)
                 notifySubscribers()
+                async(CommonPool) { songDatabase.songDao().updateData(data) }
             },
-            onFailure = {
-                onFailure()
-            })
+            onFailure = { onFailure() })
     }
 }
