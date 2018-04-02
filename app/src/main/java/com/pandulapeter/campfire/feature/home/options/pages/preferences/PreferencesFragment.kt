88d68a12b1f4aa281543cf7dbd2257238ee6e590
Péter Pandula <peter@halcyonmobile.com>
2018-04-02 13:02:29 +0300

    Finalize the preferences screen.

--- app/src/main/java/com/pandulapeter/campfire/feature/home/options/pages/preferences/PreferencesFragment.kt
+++ app/src/main/java/com/pandulapeter/campfire/feature/home/options/pages/preferences/PreferencesFragment.kt
@@ -5,13 +5,37 @@ import android.view.View
 import com.pandulapeter.campfire.R
 import com.pandulapeter.campfire.databinding.FragmentOptionsPreferencesBinding
 import com.pandulapeter.campfire.feature.shared.CampfireFragment
+import com.pandulapeter.campfire.feature.shared.dialog.AlertDialogFragment
+import com.pandulapeter.campfire.old.util.onEventTriggered
 import com.pandulapeter.campfire.util.onPropertyChanged
 
-class PreferencesFragment : CampfireFragment<FragmentOptionsPreferencesBinding, PreferencesViewModel>(R.layout.fragment_options_preferences) {
+class PreferencesFragment : CampfireFragment<FragmentOptionsPreferencesBinding, PreferencesViewModel>(R.layout.fragment_options_preferences),
+    AlertDialogFragment.OnDialogItemsSelectedListener {
+
+    companion object {
+        private const val DIALOG_ID_RESET_HINTS_CONFIRMATION = 3
+    }
 
     override val viewModel = PreferencesViewModel()
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
         viewModel.shouldUseDarkTheme.onPropertyChanged(this) { activity?.recreate() }
+        viewModel.shouldShowHintsResetConfirmation.onEventTriggered(this) {
+            AlertDialogFragment.show(
+                DIALOG_ID_RESET_HINTS_CONFIRMATION,
+                childFragmentManager,
+                R.string.settings_reset_hints_confirmation_title,
+                R.string.settings_reset_hints_confirmation_message,
+                R.string.settings_reset_hints_confirmation_reset,
+                R.string.cancel
+            )
+        }
+        viewModel.shouldShowHintsResetSnackbar.onEventTriggered(this) { showSnackbar(R.string.settings_reset_hints_message) }
+    }
+
+    override fun onPositiveButtonSelected(id: Int) {
+        if (id == DIALOG_ID_RESET_HINTS_CONFIRMATION) {
+            viewModel.resetHints()
+        }
     }
 }
--- app/src/main/java/com/pandulapeter/campfire/feature/home/options/pages/preferences/PreferencesViewModel.kt
+++ app/src/main/java/com/pandulapeter/campfire/feature/home/options/pages/preferences/PreferencesViewModel.kt
@@ -4,18 +4,22 @@ import android.databinding.ObservableBoolean
 import com.pandulapeter.campfire.data.model.local.Note
 import com.pandulapeter.campfire.data.persistence.PreferenceDatabase
 import com.pandulapeter.campfire.feature.shared.CampfireViewModel
+import com.pandulapeter.campfire.integration.FirstTimeUserExperienceManager
 import com.pandulapeter.campfire.util.onPropertyChanged
 import org.koin.android.ext.android.inject
 
 class PreferencesViewModel : CampfireViewModel() {
 
     private val preferenceDatabase by inject<PreferenceDatabase>()
+    private val firstTimeUserExperienceManager by inject<FirstTimeUserExperienceManager>()
     val shouldShowChords = ObservableBoolean(preferenceDatabase.shouldShowChords)
     val shouldUseGermanNotation = ObservableBoolean(preferenceDatabase.shouldUseGermanNotation)
     val englishNotationExample = generateNotationExample(false)
     val germanNotationExample = generateNotationExample(true)
     val shouldUseDarkTheme = ObservableBoolean(preferenceDatabase.shouldUseDarkTheme)
     val shouldShowExitConfirmation = ObservableBoolean(preferenceDatabase.shouldShowExitConfirmation)
+    val shouldShowHintsResetConfirmation = ObservableBoolean()
+    val shouldShowHintsResetSnackbar = ObservableBoolean()
     val shouldShareUsageData = ObservableBoolean(preferenceDatabase.shouldShareUsageData)
 
     init {
@@ -26,7 +30,14 @@ class PreferencesViewModel : CampfireViewModel() {
         shouldShareUsageData.onPropertyChanged { preferenceDatabase.shouldShareUsageData = it }
     }
 
-    fun onResetHintsClicked() = Unit
+    fun onResetHintsClicked() {
+        shouldShowHintsResetConfirmation.set(true)
+    }
+
+    fun resetHints() {
+        firstTimeUserExperienceManager.resetAll()
+        shouldShowHintsResetSnackbar.set(true)
+    }
 
     private fun generateNotationExample(shouldUseGermanNotation: Boolean) = listOf(
         Note.C.getName(shouldUseGermanNotation),
