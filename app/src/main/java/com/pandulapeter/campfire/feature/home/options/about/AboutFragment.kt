2e61bf16178a35a0f67dfd888efa2ced69ab3c9f
Péter Pandula <peter@halcyonmobile.com>
2018-04-08 13:43:11 +0300

    Implement functionality on the About screen.

--- app/src/main/java/com/pandulapeter/campfire/feature/home/options/about/AboutFragment.kt
+++ app/src/main/java/com/pandulapeter/campfire/feature/home/options/about/AboutFragment.kt
@@ -1,10 +1,17 @@
 package com.pandulapeter.campfire.feature.home.options.about
 
+import android.os.Bundle
+import android.view.View
 import com.pandulapeter.campfire.R
 import com.pandulapeter.campfire.databinding.FragmentOptionsAboutBinding
 import com.pandulapeter.campfire.feature.shared.CampfireFragment
+import com.pandulapeter.campfire.old.util.onEventTriggered
 
 class AboutFragment : CampfireFragment<FragmentOptionsAboutBinding, AboutViewModel>(R.layout.fragment_options_about) {
 
     override val viewModel = AboutViewModel()
+
+    override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
+        viewModel.shouldErrorShowSnackbar.onEventTriggered { showSnackbar(R.string.options_about_error) }
+    }
 }
--- app/src/main/java/com/pandulapeter/campfire/feature/home/options/about/AboutViewModel.kt
+++ app/src/main/java/com/pandulapeter/campfire/feature/home/options/about/AboutViewModel.kt
@@ -1,14 +1,53 @@
 package com.pandulapeter.campfire.feature.home.options.about
 
+import android.content.ActivityNotFoundException
+import android.content.Context
+import android.content.Intent
+import android.databinding.ObservableBoolean
+import android.net.Uri
+import com.pandulapeter.campfire.R
 import com.pandulapeter.campfire.feature.shared.CampfireViewModel
 
 class AboutViewModel : CampfireViewModel() {
 
-    fun onGooglePlayClicked() = Unit
+    companion object {
+        private const val PLAY_STORE_URL = "market://details?id=com.pandulapeter.campfire"
+        private const val GIT_HUB_URL = "https://github.com/pandulapeter/campfire-android"
+        private const val EMAIL_ADDRESS = "pandulapeter@gmail.com"
+    }
 
-    fun onGitHubClicked() = Unit
+    val shouldErrorShowSnackbar = ObservableBoolean()
 
-    fun onShareClicked() = Unit
+    fun onGooglePlayClicked(context: Context) = context.tryToOpenIntent(PLAY_STORE_URL.toUrlIntent())
 
-    fun onContactClicked() = Unit
+    fun onGitHubClicked(context: Context) = context.tryToOpenIntent(GIT_HUB_URL.toUrlIntent())
+
+    fun onShareClicked(context: Context) = context.tryToOpenIntent(
+        Intent.createChooser(
+            Intent().apply {
+                action = Intent.ACTION_SEND
+                type = "text/plain"
+            }.putExtra(Intent.EXTRA_TEXT, PLAY_STORE_URL).addFlags(Intent.FLAG_ACTIVITY_NEW_TASK), null
+        )
+    )
+
+    fun onContactClicked(context: Context) = context.tryToOpenIntent(
+        Intent.createChooser(
+            Intent().apply {
+                action = Intent.ACTION_SENDTO
+                type = "text/plain"
+                data = Uri.parse("mailto:$EMAIL_ADDRESS?subject=${Uri.encode(context.getString(R.string.campfire))}")
+            }.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK), null
+        )
+    )
+
+    private fun Context.tryToOpenIntent(intent: Intent) {
+        try {
+            startActivity(intent)
+        } catch (exception: ActivityNotFoundException) {
+            shouldErrorShowSnackbar.set(true)
+        }
+    }
+
+    private fun String.toUrlIntent() = Intent(Intent.ACTION_VIEW).apply { data = Uri.parse(this@toUrlIntent) }
 }
