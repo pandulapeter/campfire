ffcc93786684a8fe1deccdf0a2c6de038b5db0b4
Péter Pandula <peter@halcyonmobile.com>
2018-04-09 20:32:25 +0300

    Remove HTTP logging interceptor.

--- app/src/main/java/com/pandulapeter/campfire/data/networking/NetworkManager.kt
+++ app/src/main/java/com/pandulapeter/campfire/data/networking/NetworkManager.kt
@@ -1,9 +1,7 @@
 package com.pandulapeter.campfire.data.networking
 
 import com.google.gson.Gson
-import com.pandulapeter.campfire.BuildConfig
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -11,11 +9,7 @@ class NetworkManager(gson: Gson) {
 
     val service: CampfireService = Retrofit.Builder()
         .baseUrl("https://campfire-test1.herokuapp.com/")
-        .client(OkHttpClient.Builder().apply {
-            if (BuildConfig.DEBUG) {
-                addInterceptor(HttpLoggingInterceptor().apply { level = HttpLoggingInterceptor.Level.BODY })
-            }
-        }.build())
+        .client(OkHttpClient.Builder().build())
         .addConverterFactory(GsonConverterFactory.create(gson))
         .build()
         .create(CampfireService::class.java)
