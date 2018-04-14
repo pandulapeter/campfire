a0a0c43cf2b3b4db94683450afa1c94c22514626
Péter Pandula <peter@halcyonmobile.com>
2018-04-14 07:49:54 +0300

    Improve return transition from the detail screen.

--- app/src/main/java/com/pandulapeter/campfire/feature/CampfireActivity.kt
+++ app/src/main/java/com/pandulapeter/campfire/feature/CampfireActivity.kt
@@ -379,6 +379,7 @@ class CampfireActivity : AppCompatActivity(), AlertDialogFragment.OnDialogItemsS
                 reenterTransition = null
             }
         }
+        binding.coordinatorLayout.layoutTransition.disableTransitionType(LayoutTransition.CHANGING)
         supportFragmentManager.beginTransaction()
             .setAllowOptimization(true)
             .replace(R.id.fragment_container, DetailFragment.newInstance(songs, index, shouldShowManagePlaylist))
