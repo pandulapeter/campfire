4f2af60170c525b1c728046cf303f8b37f6f058e
Péter Pandula <peter@halcyonmobile.com>
2018-04-14 11:44:53 +0300

    Open the New Playlist dialog from the navigation drawer.

--- app/src/main/java/com/pandulapeter/campfire/feature/shared/CampfireFragment.kt
+++ app/src/main/java/com/pandulapeter/campfire/feature/shared/CampfireFragment.kt
@@ -82,7 +82,7 @@ abstract class CampfireFragment<B : ViewDataBinding, out VM : CampfireViewModel>
 
     protected fun isSnackbarVisible() = snackbar?.isShownOrQueued ?: false
 
-    protected fun hideSnackbar() = snackbar?.dismiss()
+    fun hideSnackbar() = snackbar?.dismiss()
 
     protected fun showSnackbar(@StringRes message: Int, @StringRes actionText: Int = R.string.try_again, action: (() -> Unit)? = null, dismissAction: (() -> Unit)? = null) =
         showSnackbar(getString(message), actionText, action, dismissAction)
