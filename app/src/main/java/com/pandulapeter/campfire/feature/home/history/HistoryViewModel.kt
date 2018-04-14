592801c7ca3ea4f2bcef3f4db3d3f795d8b0a9d7
Péter Pandula <peter@halcyonmobile.com>
2018-04-14 03:35:05 +0300

    Re-implement headers on the History screen.

--- app/src/main/java/com/pandulapeter/campfire/feature/home/history/HistoryViewModel.kt
+++ app/src/main/java/com/pandulapeter/campfire/feature/home/history/HistoryViewModel.kt
@@ -43,7 +43,20 @@ class HistoryViewModel(context: Context, private val openLibrary: () -> Unit) : 
         .filter { song -> history.firstOrNull { it.id == song.id } != null }
         .sortedByDescending { song -> history.first { it.id == song.id }.lastOpenedAt }
         .map { SongListItemViewModel.SongViewModel(context, songDetailRepository, it) }
-        .toList()
+        .toMutableList<SongListItemViewModel>()
+        .apply {
+            val headerIndices = mutableListOf<Int>()
+            val songsOnly = filterIsInstance<SongListItemViewModel.SongViewModel>().map { it.song }
+            songsOnly.forEachIndexed { index, song ->
+                if (index == 0 || getHeaderTitle(index, songsOnly) != getHeaderTitle(index - 1, songsOnly)) {
+                    headerIndices.add(index)
+                }
+            }
+            (headerIndices.size - 1 downTo 0).forEach { position ->
+                val index = headerIndices[position]
+                add(index, SongListItemViewModel.HeaderViewModel(context.getString(getHeaderTitle(index, songsOnly))))
+            }
+        }
 
     override fun onListUpdated(items: List<SongListItemViewModel>) {
         super.onListUpdated(items)
@@ -57,39 +70,37 @@ class HistoryViewModel(context: Context, private val openLibrary: () -> Unit) : 
         updateAdapterItems(true)
     }
 
-//    fun isHeader(position: Int) = position == 0 || getHeaderTitle(position) != getHeaderTitle(position - 1)
-//
-//    fun getHeaderTitle(position: Int): Int {
-//        val timestamp = history.firstOrNull { it.id == adapter.items[position].song.id }?.lastOpenedAt ?: 0L
-//        if (timestamp == 0L) {
-//            return 0
-//        }
-//        val now = Calendar.getInstance()
-//        val then = Calendar.getInstance().apply { timeInMillis = timestamp }
-//        if (Math.abs(now.timeInMillis - then.timeInMillis) < 30 * 60 * 1000) {
-//            return R.string.history_now
-//        }
-//        if (now.year == then.year && now.month == then.month && now.day == then.day) {
-//            return R.string.history_today
-//        }
-//        val yesterday = Calendar.getInstance().apply { timeInMillis -= 24 * 60 * 60 * 1000 }
-//        if (yesterday.year == then.year && yesterday.month == then.month && yesterday.day == then.day) {
-//            return R.string.history_yesterday
-//        }
-//        return when (Math.abs(now.year - then.year)) {
-//            0 -> when (Math.abs(now.month - then.month)) {
-//                0 -> when (Math.abs(now.week - then.week)) {
-//                    0 -> R.string.history_this_week
-//                    1 -> R.string.history_last_week
-//                    else -> R.string.history_this_month
-//                }
-//                1 -> R.string.history_last_month
-//                else -> R.string.history_this_year
-//            }
-//            1 -> R.string.history_last_year
-//            else -> R.string.history_a_long_time_ago
-//        }
-//    }
+    private fun getHeaderTitle(position: Int, songs: List<Song>): Int {
+        val timestamp = history.firstOrNull { it.id == songs[position].id }?.lastOpenedAt ?: 0L
+        if (timestamp == 0L) {
+            return 0
+        }
+        val now = Calendar.getInstance()
+        val then = Calendar.getInstance().apply { timeInMillis = timestamp }
+        if (Math.abs(now.timeInMillis - then.timeInMillis) < 30 * 60 * 1000) {
+            return R.string.history_now
+        }
+        if (now.year == then.year && now.month == then.month && now.day == then.day) {
+            return R.string.history_today
+        }
+        val yesterday = Calendar.getInstance().apply { timeInMillis -= 24 * 60 * 60 * 1000 }
+        if (yesterday.year == then.year && yesterday.month == then.month && yesterday.day == then.day) {
+            return R.string.history_yesterday
+        }
+        return when (Math.abs(now.year - then.year)) {
+            0 -> when (Math.abs(now.month - then.month)) {
+                0 -> when (Math.abs(now.week - then.week)) {
+                    0 -> R.string.history_this_week
+                    1 -> R.string.history_last_week
+                    else -> R.string.history_this_month
+                }
+                1 -> R.string.history_last_month
+                else -> R.string.history_this_year
+            }
+            1 -> R.string.history_last_year
+            else -> R.string.history_a_long_time_ago
+        }
+    }
 
     fun deleteAllSongs() = historyRepository.deleteAllHistory()
 
