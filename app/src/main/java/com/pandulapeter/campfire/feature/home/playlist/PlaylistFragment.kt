5a4df7644faf8c192ca8f593291a03a868037eb6
Péter Pandula <peter@halcyonmobile.com>
2018-04-16 17:59:50 +0300

    Start working on the Playlist edit mode.

--- app/src/main/java/com/pandulapeter/campfire/feature/home/playlist/PlaylistFragment.kt
+++ app/src/main/java/com/pandulapeter/campfire/feature/home/playlist/PlaylistFragment.kt
@@ -1,12 +1,14 @@
 package com.pandulapeter.campfire.feature.home.playlist
 
+import android.content.Context
 import android.os.Bundle
 import android.view.View
 import com.pandulapeter.campfire.R
+import com.pandulapeter.campfire.data.model.local.Playlist
 import com.pandulapeter.campfire.feature.home.shared.songList.SongListFragment
-import com.pandulapeter.campfire.util.BundleArgumentDelegate
-import com.pandulapeter.campfire.util.onPropertyChanged
-import com.pandulapeter.campfire.util.withArguments
+import com.pandulapeter.campfire.feature.shared.widget.ToolbarButton
+import com.pandulapeter.campfire.feature.shared.widget.ToolbarTextInputView
+import com.pandulapeter.campfire.util.*
 
 class PlaylistFragment : SongListFragment<PlaylistViewModel>() {
 
@@ -20,17 +22,52 @@ class PlaylistFragment : SongListFragment<PlaylistViewModel>() {
         PlaylistViewModel(
             context = mainActivity,
             playlistId = arguments.playlistId,
-            openLibrary = { mainActivity.openLibraryScreen() })
+            openLibrary = { mainActivity.openLibraryScreen() },
+            toolbarTextInputView = if (arguments?.playlistId == Playlist.FAVORITES_ID) null else ToolbarTextInputView(mainActivity.toolbarContext),
+            onDataLoaded = {
+                if (it) {
+                    mainActivity.updateToolbarButtons(listOf(editToggle))
+                }
+            }
+        )
     }
+    private var Bundle.isInEditMode by BundleArgumentDelegate.Boolean("isInEditMode")
+    private val editToggle: ToolbarButton by lazy { mainActivity.toolbarContext.createToolbarButton(R.drawable.ic_edit_24dp) { viewModel.toggleEditMode() } }
+    private val drawableEditToDone by lazy { mainActivity.animatedDrawable(R.drawable.avd_edit_to_done_24dp) }
+    private val drawableDoneToEdit by lazy { mainActivity.animatedDrawable(R.drawable.avd_done_to_edit_24dp) }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.swipeRefreshLayout.isEnabled = false
         viewModel.songCount.onPropertyChanged(this) { updateToolbarTitle(it) }
         viewModel.playlist.onPropertyChanged(this) { updateToolbarTitle() }
+        viewModel.isInEditMode.onPropertyChanged {
+            editToggle.setImageDrawable((if (it) drawableEditToDone else drawableDoneToEdit)?.apply { start() })
+            mainActivity.shouldAllowAppBarScrolling = !it
+            mainActivity.transitionMode = true
+        }
+        savedInstanceState?.let {
+            if (it.isInEditMode) {
+                viewModel.isInEditMode.set(true)
+                editToggle.setImageDrawable(mainActivity.drawable(R.drawable.ic_done_24dp))
+                viewModel.toolbarTextInputView?.textInput?.run {
+                    setText(viewModel.playlist.get()?.title)
+                    setSelection(text.length)
+                }
+                viewModel.toolbarTextInputView?.showTextInput()
+            }
+        }
+        viewModel.toolbarTextInputView?.textInput?.requestFocus()
     }
 
-    private fun updateToolbarTitle(songCount: Int = viewModel.songCount.get()) = defaultToolbar.updateToolbarTitle(
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.isInEditMode = viewModel.isInEditMode.get()
+    }
+
+    override fun inflateToolbarTitle(context: Context) = viewModel.toolbarTextInputView ?: defaultToolbar
+
+    private fun updateToolbarTitle(songCount: Int = viewModel.songCount.get()) = (viewModel.toolbarTextInputView?.title ?: defaultToolbar).updateToolbarTitle(
         viewModel.playlist.get()?.title ?: getString(R.string.home_favorites),
         if (songCount == 0) getString(R.string.manage_playlists_song_count_empty) else mainActivity.resources.getQuantityString(R.plurals.playlist_song_count, songCount, songCount)
     )
--- app/src/main/java/com/pandulapeter/campfire/feature/home/playlist/PlaylistViewModel.kt
+++ app/src/main/java/com/pandulapeter/campfire/feature/home/playlist/PlaylistViewModel.kt
@@ -1,6 +1,7 @@
 package com.pandulapeter.campfire.feature.home.playlist
 
 import android.content.Context
+import android.databinding.ObservableBoolean
 import android.databinding.ObservableField
 import android.databinding.ObservableInt
 import com.pandulapeter.campfire.R
@@ -8,16 +9,20 @@ import com.pandulapeter.campfire.data.model.local.Playlist
 import com.pandulapeter.campfire.data.model.remote.Song
 import com.pandulapeter.campfire.feature.home.shared.songList.SongListItemViewModel
 import com.pandulapeter.campfire.feature.home.shared.songList.SongListViewModel
+import com.pandulapeter.campfire.feature.shared.widget.ToolbarTextInputView
 
 class PlaylistViewModel(
     context: Context,
     private val playlistId: String,
-    private val openLibrary: () -> Unit
+    private val openLibrary: () -> Unit,
+    val toolbarTextInputView: ToolbarTextInputView?,
+    private val onDataLoaded: (Boolean) -> Unit
 ) : SongListViewModel(context) {
 
     var playlist = ObservableField<Playlist?>()
     private var songToDeleteId: String? = null
     val songCount = ObservableInt()
+    val isInEditMode = ObservableBoolean()
 
     init {
         placeholderText.set(R.string.playlist_placeholder)
@@ -42,11 +47,28 @@ class PlaylistViewModel(
 
     override fun onPlaylistsUpdated(playlists: List<Playlist>) {
         super.onPlaylistsUpdated(playlists)
-        playlist.set(playlists.findLast { it.id == playlistId })
+        playlists.findLast { it.id == playlistId }.let {
+            playlist.set(it)
+            onDataLoaded(playlistId != Playlist.FAVORITES_ID || (it?.songIds?.size ?: 0) > 1)
+        }
     }
 
     override fun onListUpdated(items: List<SongListItemViewModel>) {
         super.onListUpdated(items)
         songCount.set(items.size)
+    }
+
+    fun toggleEditMode() {
+        toolbarTextInputView?.run {
+            if (title.tag == null) {
+                animateTextInputVisibility(!isTextInputVisible)
+                if (isTextInputVisible) {
+                    textInput.setText("")
+                }
+                this@PlaylistViewModel.isInEditMode.set(toolbarTextInputView.isTextInputVisible)
+            }
+            return
+        }
+        isInEditMode.set(!isInEditMode.get())
     }
 }
