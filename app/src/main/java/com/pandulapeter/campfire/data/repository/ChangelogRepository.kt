fdec523d3fafecf0105d305816402aa22d109de5
Péter Pandula <peter@halcyonmobile.com>
2018-04-21 17:12:15 +0300

    Increment version number to v0.0.4.

--- app/src/main/java/com/pandulapeter/campfire/data/repository/ChangelogRepository.kt
+++ app/src/main/java/com/pandulapeter/campfire/data/repository/ChangelogRepository.kt
@@ -6,6 +6,7 @@ import com.pandulapeter.campfire.data.model.local.ChangelogItem
 class ChangelogRepository {
 
     val data = listOf(
+        ChangelogItem(R.string.options_changelog_0_0_4_version_name, R.string.options_changelog_0_0_4_description),
         ChangelogItem(R.string.options_changelog_0_0_3_version_name, R.string.options_changelog_0_0_3_description),
         ChangelogItem(R.string.options_changelog_0_0_2_version_name, R.string.options_changelog_0_0_2_description),
         ChangelogItem(R.string.options_changelog_0_0_1_version_name, R.string.options_changelog_0_0_1_description)
