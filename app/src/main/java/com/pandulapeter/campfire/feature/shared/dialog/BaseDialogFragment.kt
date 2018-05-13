4c6a14e89eed7ba99b124cb43ad88ca9f6e41b6c
Péter Pandula <peter@halcyonmobile.com>
2018-05-13 10:27:31 +0300

    Fix dialog position not being adjusted when the keyboard is present.

--- app/src/main/java/com/pandulapeter/campfire/feature/shared/dialog/BaseDialogFragment.kt
+++ app/src/main/java/com/pandulapeter/campfire/feature/shared/dialog/BaseDialogFragment.kt
@@ -3,12 +3,14 @@ package com.pandulapeter.campfire.feature.shared.dialog
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatDialogFragment
+import com.pandulapeter.campfire.R
 
 abstract class BaseDialogFragment : AppCompatDialogFragment() {
 
     protected val onDialogItemSelectedListener get() = parentFragment as? OnDialogItemSelectedListener ?: activity as? OnDialogItemSelectedListener
 
-    override fun onCreateDialog(savedInstanceState: Bundle?) = context?.let { AlertDialog.Builder(it).createDialog(arguments) } ?: super.onCreateDialog(savedInstanceState)
+    override fun onCreateDialog(savedInstanceState: Bundle?) =
+        context?.let { AlertDialog.Builder(it, R.style.DialogTheme).createDialog(arguments) } ?: super.onCreateDialog(savedInstanceState)
 
     abstract fun AlertDialog.Builder.createDialog(arguments: Bundle?): AlertDialog
 
