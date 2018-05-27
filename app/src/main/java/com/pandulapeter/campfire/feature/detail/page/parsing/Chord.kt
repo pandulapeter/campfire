0228f64083659c5310828dcfe4d3a8e6b9a8906d
Péter Pandula <peter@halcyonmobile.com>
2018-05-27 14:44:19 +0300

    Improve SongParser.

--- app/src/main/java/com/pandulapeter/campfire/feature/detail/page/parsing/Chord.kt
+++ app/src/main/java/com/pandulapeter/campfire/feature/detail/page/parsing/Chord.kt
@@ -9,6 +9,6 @@ data class Chord(
     private val bassNote: Note? = if (suffix.indexOf('/') >= 0) Note.toNote("[" + suffix.substring(suffix.indexOf('/') + 1) + "]") else null
     val isHint = root === Note.Hint
 
-    fun getName(transposition: Int, shouldUseGermanNotation: Boolean) =
-        root.transpose(transposition).getName(shouldUseGermanNotation) + if (bassNote == null) suffix else "/" + bassNote.transpose(transposition).getName(shouldUseGermanNotation)
+    fun getName(transposition: Int, shouldUseGermanNotation: Boolean) = root.transpose(transposition).getName(shouldUseGermanNotation) +
+            if (bassNote == null) suffix else "${suffix.substring(0, suffix.indexOf('/'))}/${bassNote.transpose(transposition).getName(shouldUseGermanNotation)}"
 }
