951ac55b7f164b751afdff50d3358d59ff5dda9d
Péter Pandula <peter@halcyonmobile.com>
2018-06-06 10:53:49 +0300

    Implement language change and theme change.

--- app/src/main/java/com/pandulapeter/campfire/feature/main/home/onboarding/page/welcome/WelcomeFragment.kt
+++ app/src/main/java/com/pandulapeter/campfire/feature/main/home/onboarding/page/welcome/WelcomeFragment.kt
@@ -5,10 +5,16 @@ import android.view.View
 import android.widget.FrameLayout
 import com.pandulapeter.campfire.R
 import com.pandulapeter.campfire.databinding.FragmentOnboardingWelcomeBinding
+import com.pandulapeter.campfire.feature.main.options.preferences.LanguageSelectorBottomSheetFragment
+import com.pandulapeter.campfire.feature.main.options.preferences.PreferencesViewModel
+import com.pandulapeter.campfire.feature.main.options.preferences.ThemeSelectorBottomSheetFragment
 import com.pandulapeter.campfire.feature.shared.CampfireFragment
-import com.pandulapeter.campfire.util.waitForLayout
+import com.pandulapeter.campfire.util.onEventTriggered
+import com.pandulapeter.campfire.util.onPropertyChanged
 
-class WelcomeFragment : CampfireFragment<FragmentOnboardingWelcomeBinding, WelcomeViewModel>(R.layout.fragment_onboarding_welcome) {
+class WelcomeFragment : CampfireFragment<FragmentOnboardingWelcomeBinding, WelcomeViewModel>(R.layout.fragment_onboarding_welcome),
+    ThemeSelectorBottomSheetFragment.OnThemeSelectedListener,
+    LanguageSelectorBottomSheetFragment.OnLanguageSelectedListener {
 
     override val viewModel = WelcomeViewModel()
 
@@ -18,5 +24,23 @@ class WelcomeFragment : CampfireFragment<FragmentOnboardingWelcomeBinding, Welco
                 layoutParams = (layoutParams as FrameLayout.LayoutParams).apply { setMargins(0, -getCampfireActivity().toolbarHeight, 0, 0) }
             }
         }
+        viewModel.apply {
+            shouldShowLanguageSelector.onEventTriggered {
+                if (!getCampfireActivity().isUiBlocked) {
+                    language.get()?.let { LanguageSelectorBottomSheetFragment.show(childFragmentManager, it.id) }
+                }
+            }
+            language.onPropertyChanged(this@WelcomeFragment) { binding.root.post { if (isAdded) getCampfireActivity().recreate() } }
+            shouldShowThemeSelector.onEventTriggered {
+                if (!getCampfireActivity().isUiBlocked) {
+                    theme.get()?.let { ThemeSelectorBottomSheetFragment.show(childFragmentManager, it.id) }
+                }
+            }
+            theme.onPropertyChanged(this@WelcomeFragment) { binding.root.post { if (isAdded) getCampfireActivity().recreate() } }
+        }
     }
+
+    override fun onThemeSelected(theme: PreferencesViewModel.Theme) = viewModel.theme.set(theme)
+
+    override fun onLanguageSelected(language: PreferencesViewModel.Language) = viewModel.language.set(language)
 }
--- app/src/main/java/com/pandulapeter/campfire/feature/main/home/onboarding/page/welcome/WelcomeViewModel.kt
+++ app/src/main/java/com/pandulapeter/campfire/feature/main/home/onboarding/page/welcome/WelcomeViewModel.kt
@@ -1,5 +1,55 @@
 package com.pandulapeter.campfire.feature.main.home.onboarding.page.welcome
 
+import android.databinding.ObservableBoolean
+import android.databinding.ObservableField
+import com.pandulapeter.campfire.data.persistence.PreferenceDatabase
+import com.pandulapeter.campfire.feature.main.options.preferences.PreferencesViewModel
 import com.pandulapeter.campfire.feature.shared.CampfireViewModel
+import com.pandulapeter.campfire.util.onPropertyChanged
+import org.koin.android.ext.android.inject
 
-class WelcomeViewModel : CampfireViewModel()
+class WelcomeViewModel : CampfireViewModel() {
+    private val preferenceDatabase by inject<PreferenceDatabase>()
+    val shouldShowThemeSelector = ObservableBoolean()
+    val shouldShowLanguageSelector = ObservableBoolean()
+    val theme = ObservableField<PreferencesViewModel.Theme>(PreferencesViewModel.Theme.fromId(preferenceDatabase.theme))
+    val themeDescription = ObservableField("")
+    val language = ObservableField<PreferencesViewModel.Language>(PreferencesViewModel.Language.fromId(preferenceDatabase.language))
+    val languageDescription = ObservableField("")
+
+    init {
+        theme.onPropertyChanged {
+            preferenceDatabase.theme = it.id
+            updateThemeDescription()
+        }
+        language.onPropertyChanged {
+            preferenceDatabase.language = it.id
+            updateLanguageDescription()
+        }
+    }
+
+    fun onThemeClicked() = shouldShowThemeSelector.set(true)
+
+    fun onLanguageClicked() = shouldShowLanguageSelector.set(true)
+
+    private fun updateThemeDescription() = Unit //themeDescription.set(
+//        context.getString(
+//            when (theme.get()) {
+//                null, PreferencesViewModel.Theme.AUTOMATIC -> R.string.options_preferences_app_theme_automatic_description
+//                PreferencesViewModel.Theme.DARK -> R.string.options_preferences_app_theme_dark_description
+//                PreferencesViewModel.Theme.LIGHT -> R.string.options_preferences_app_theme_light_description
+//            }
+//        )
+//    )
+
+    private fun updateLanguageDescription() = Unit //languageDescription.set(
+//        context.getString(
+//            when (language.get()) {
+//                null, PreferencesViewModel.Language.AUTOMATIC -> R.string.options_preferences_language_automatic_description
+//                PreferencesViewModel.Language.ENGLISH -> R.string.options_preferences_language_english_description
+//                PreferencesViewModel.Language.HUNGARIAN -> R.string.options_preferences_language_hungarian_description
+//                PreferencesViewModel.Language.ROMANIAN -> R.string.options_preferences_language_romanian_description
+//            }
+//        )
+//    )
+}
