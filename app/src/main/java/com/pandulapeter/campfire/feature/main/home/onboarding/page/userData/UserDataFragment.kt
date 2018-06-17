a43f7a74b8c4fb6cae9eb7250ca85e68f93c35fd
Péter Pandula <peter@halcyonmobile.com>
2018-06-17 14:58:48 +0300

    Move the Privacy Policy and the Terms of Service links to the first page.

--- app/src/main/java/com/pandulapeter/campfire/feature/main/home/onboarding/page/userData/UserDataFragment.kt
+++ app/src/main/java/com/pandulapeter/campfire/feature/main/home/onboarding/page/userData/UserDataFragment.kt
@@ -1,47 +1,10 @@
 package com.pandulapeter.campfire.feature.main.home.onboarding.page.userData
 
-import android.net.Uri
-import android.os.Bundle
-import android.support.customtabs.CustomTabsIntent
-import android.text.SpannableString
-import android.text.Spanned
-import android.text.method.LinkMovementMethod
-import android.text.style.ClickableSpan
-import android.view.View
 import com.pandulapeter.campfire.R
 import com.pandulapeter.campfire.databinding.FragmentOnboardingUserDataBinding
-import com.pandulapeter.campfire.feature.main.options.about.AboutViewModel
 import com.pandulapeter.campfire.feature.shared.CampfireFragment
-import com.pandulapeter.campfire.util.color
 
 class UserDataFragment : CampfireFragment<FragmentOnboardingUserDataBinding, UserDataViewModel>(R.layout.fragment_onboarding_user_data) {
 
     override val viewModel = UserDataViewModel()
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        val first = getString(R.string.user_data_message_end_part_1)
-        val second = getString(R.string.user_data_message_end_part_2)
-        val third = getString(R.string.user_data_message_end_part_3)
-        val fourth = getString(R.string.user_data_message_end_part_4)
-        val fifth = getString(R.string.user_data_message_end_part_5)
-        binding.textBottom.movementMethod = LinkMovementMethod.getInstance()
-        binding.textBottom.text = SpannableString("$first$second$third$fourth$fifth").apply {
-            setSpan(object : ClickableSpan() {
-                override fun onClick(widget: View?) {
-                    CustomTabsIntent.Builder()
-                        .setToolbarColor(requireContext().color(R.color.accent))
-                        .build()
-                        .launchUrl(requireContext(), Uri.parse(AboutViewModel.PRIVACY_POLICY_URL))
-                }
-            }, first.length, first.length + second.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-            setSpan(object : ClickableSpan() {
-                override fun onClick(widget: View?) {
-                    CustomTabsIntent.Builder()
-                        .setToolbarColor(requireContext().color(R.color.accent))
-                        .build()
-                        .launchUrl(requireContext(), Uri.parse(AboutViewModel.TERMS_AND_CONDITIONS_URL))
-                }
-            }, first.length + second.length + third.length, length - fifth.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-        }
-    }
 }
--- app/src/main/java/com/pandulapeter/campfire/feature/main/home/onboarding/page/welcome/WelcomeFragment.kt
+++ app/src/main/java/com/pandulapeter/campfire/feature/main/home/onboarding/page/welcome/WelcomeFragment.kt
@@ -1,13 +1,21 @@
 package com.pandulapeter.campfire.feature.main.home.onboarding.page.welcome
 
+import android.net.Uri
 import android.os.Bundle
+import android.support.customtabs.CustomTabsIntent
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import android.view.View
 import com.pandulapeter.campfire.R
 import com.pandulapeter.campfire.databinding.FragmentOnboardingWelcomeBinding
+import com.pandulapeter.campfire.feature.main.options.about.AboutViewModel
 import com.pandulapeter.campfire.feature.main.options.preferences.LanguageSelectorBottomSheetFragment
 import com.pandulapeter.campfire.feature.main.options.preferences.PreferencesViewModel
 import com.pandulapeter.campfire.feature.main.options.preferences.ThemeSelectorBottomSheetFragment
 import com.pandulapeter.campfire.feature.shared.CampfireFragment
+import com.pandulapeter.campfire.util.color
 import com.pandulapeter.campfire.util.onEventTriggered
 import com.pandulapeter.campfire.util.onPropertyChanged
 
@@ -35,6 +43,30 @@ class WelcomeFragment : CampfireFragment<FragmentOnboardingWelcomeBinding, Welco
         viewModel.theme.onPropertyChanged(this@WelcomeFragment) {
             getCampfireActivity().isUiBlocked = true
             binding.root.post { if (isAdded) getCampfireActivity().recreate() }
+        }
+        val first = getString(R.string.welcome_conditions_part_1)
+        val second = getString(R.string.welcome_conditions_part_2)
+        val third = getString(R.string.welcome_conditions_part_3)
+        val fourth = getString(R.string.welcome_conditions_part_4)
+        val fifth = getString(R.string.welcome_conditions_part_5)
+        binding.textBottom.movementMethod = LinkMovementMethod.getInstance()
+        binding.textBottom.text = SpannableString("$first$second$third$fourth$fifth").apply {
+            setSpan(object : ClickableSpan() {
+                override fun onClick(widget: View?) {
+                    CustomTabsIntent.Builder()
+                        .setToolbarColor(requireContext().color(R.color.accent))
+                        .build()
+                        .launchUrl(requireContext(), Uri.parse(AboutViewModel.PRIVACY_POLICY_URL))
+                }
+            }, first.length, first.length + second.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+            setSpan(object : ClickableSpan() {
+                override fun onClick(widget: View?) {
+                    CustomTabsIntent.Builder()
+                        .setToolbarColor(requireContext().color(R.color.accent))
+                        .build()
+                        .launchUrl(requireContext(), Uri.parse(AboutViewModel.TERMS_AND_CONDITIONS_URL))
+                }
+            }, first.length + second.length + third.length, length - fifth.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
         }
     }
 
