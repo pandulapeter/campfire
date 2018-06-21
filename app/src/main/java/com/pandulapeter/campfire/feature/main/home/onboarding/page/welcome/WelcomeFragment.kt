fb332c604743253a7eeeaa439e305d39b7410097
Péter Pandula <peter@halcyonmobile.com>
2018-06-21 15:56:19 +0300

    Replace clickable TextViews with Buttons on the Onboarding screen.

--- app/src/main/java/com/pandulapeter/campfire/feature/main/home/onboarding/page/welcome/WelcomeFragment.kt
+++ app/src/main/java/com/pandulapeter/campfire/feature/main/home/onboarding/page/welcome/WelcomeFragment.kt
@@ -1,6 +1,9 @@
 package com.pandulapeter.campfire.feature.main.home.onboarding.page.welcome
 
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.style.ForegroundColorSpan
 import android.view.View
 import com.pandulapeter.campfire.R
 import com.pandulapeter.campfire.databinding.FragmentOnboardingWelcomeBinding
@@ -8,6 +11,7 @@ import com.pandulapeter.campfire.feature.main.home.onboarding.page.OnboardingPag
 import com.pandulapeter.campfire.feature.main.options.preferences.LanguageSelectorBottomSheetFragment
 import com.pandulapeter.campfire.feature.main.options.preferences.PreferencesViewModel
 import com.pandulapeter.campfire.feature.main.options.preferences.ThemeSelectorBottomSheetFragment
+import com.pandulapeter.campfire.util.obtainColor
 import com.pandulapeter.campfire.util.onEventTriggered
 import com.pandulapeter.campfire.util.onPropertyChanged
 
@@ -16,7 +20,10 @@ class WelcomeFragment : OnboardingPageFragment<FragmentOnboardingWelcomeBinding,
     ThemeSelectorBottomSheetFragment.OnThemeSelectedListener,
     LanguageSelectorBottomSheetFragment.OnLanguageSelectedListener {
 
-    override val viewModel by lazy { WelcomeViewModel(getCampfireActivity()) }
+    override val viewModel = WelcomeViewModel()
+    private val languageText by lazy { getString(R.string.welcome_language) }
+    private val themeText by lazy { getString(R.string.welcome_theme) }
+    private val secondaryTextColor by lazy { getCampfireActivity().obtainColor(android.R.attr.textColorSecondary) }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -28,6 +35,7 @@ class WelcomeFragment : OnboardingPageFragment<FragmentOnboardingWelcomeBinding,
         viewModel.language.onPropertyChanged(this) {
             getCampfireActivity().isUiBlocked = true
             binding.root.post { if (isAdded) getCampfireActivity().recreate() }
+            updateLanguageDescription()
         }
         viewModel.shouldShowThemeSelector.onEventTriggered(this) {
             if (!getCampfireActivity().isUiBlocked) {
@@ -37,10 +45,38 @@ class WelcomeFragment : OnboardingPageFragment<FragmentOnboardingWelcomeBinding,
         viewModel.theme.onPropertyChanged(this@WelcomeFragment) {
             getCampfireActivity().isUiBlocked = true
             binding.root.post { if (isAdded) getCampfireActivity().recreate() }
+            updateThemeDescription()
         }
+        updateLanguageDescription()
+        updateThemeDescription()
     }
 
     override fun onThemeSelected(theme: PreferencesViewModel.Theme) = viewModel.theme.set(theme)
 
     override fun onLanguageSelected(language: PreferencesViewModel.Language) = viewModel.language.set(language)
+
+    private fun updateLanguageDescription() {
+        binding.language.text = SpannableString(
+            "$languageText ${getString(
+                when (viewModel.language.get()) {
+                    null, PreferencesViewModel.Language.AUTOMATIC -> R.string.options_preferences_language_automatic
+                    PreferencesViewModel.Language.ENGLISH -> R.string.options_preferences_language_english
+                    PreferencesViewModel.Language.HUNGARIAN -> R.string.options_preferences_language_hungarian
+                    PreferencesViewModel.Language.ROMANIAN -> R.string.options_preferences_language_romanian
+                }
+            )}"
+        ).apply { setSpan(ForegroundColorSpan(secondaryTextColor), languageText.length, length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE) }
+    }
+
+    private fun updateThemeDescription() {
+        binding.theme.text = SpannableString(
+            "$themeText ${getString(
+                when (viewModel.theme.get()) {
+                    null, PreferencesViewModel.Theme.AUTOMATIC -> R.string.options_preferences_app_theme_automatic
+                    PreferencesViewModel.Theme.DARK -> R.string.options_preferences_app_theme_dark
+                    PreferencesViewModel.Theme.LIGHT -> R.string.options_preferences_app_theme_light
+                }
+            )}"
+        ).apply { setSpan(ForegroundColorSpan(secondaryTextColor), themeText.length, length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE) }
+    }
 }
--- app/src/main/java/com/pandulapeter/campfire/feature/main/home/onboarding/page/welcome/WelcomeViewModel.kt
+++ app/src/main/java/com/pandulapeter/campfire/feature/main/home/onboarding/page/welcome/WelcomeViewModel.kt
@@ -1,73 +1,29 @@
 package com.pandulapeter.campfire.feature.main.home.onboarding.page.welcome
 
-import android.content.Context
 import android.databinding.ObservableBoolean
 import android.databinding.ObservableField
 import android.text.SpannableString
-import android.text.Spanned
-import android.text.style.ForegroundColorSpan
-import com.pandulapeter.campfire.R
 import com.pandulapeter.campfire.data.persistence.PreferenceDatabase
 import com.pandulapeter.campfire.feature.main.options.preferences.PreferencesViewModel
 import com.pandulapeter.campfire.feature.shared.CampfireViewModel
-import com.pandulapeter.campfire.util.obtainColor
 import com.pandulapeter.campfire.util.onPropertyChanged
 import org.koin.android.ext.android.inject
 
-class WelcomeViewModel(private val context: Context) : CampfireViewModel() {
+class WelcomeViewModel : CampfireViewModel() {
     private val preferenceDatabase by inject<PreferenceDatabase>()
     val shouldShowThemeSelector = ObservableBoolean()
     val shouldShowLanguageSelector = ObservableBoolean()
     val language = ObservableField<PreferencesViewModel.Language>(PreferencesViewModel.Language.fromId(preferenceDatabase.language))
     val languageLabel = ObservableField(SpannableString(""))
-    private val languageText = context.getString(R.string.welcome_language)
     val theme = ObservableField<PreferencesViewModel.Theme>(PreferencesViewModel.Theme.fromId(preferenceDatabase.theme))
     val themeLabel = ObservableField(SpannableString(""))
-    private val themeText = context.getString(R.string.welcome_theme)
 
     init {
-        language.onPropertyChanged {
-            preferenceDatabase.language = it.id
-            updateLanguageDescription()
-        }
-        updateLanguageDescription()
-        theme.onPropertyChanged {
-            preferenceDatabase.theme = it.id
-            updateThemeDescription()
-        }
-        updateThemeDescription()
+        language.onPropertyChanged { preferenceDatabase.language = it.id }
+        theme.onPropertyChanged { preferenceDatabase.theme = it.id }
     }
 
     fun onThemeClicked() = shouldShowThemeSelector.set(true)
 
     fun onLanguageClicked() = shouldShowLanguageSelector.set(true)
-
-    private fun updateLanguageDescription() = languageLabel.set(
-        SpannableString(
-            "$languageText ${context.getString(
-                when (language.get()) {
-                    null, PreferencesViewModel.Language.AUTOMATIC -> R.string.options_preferences_language_automatic
-                    PreferencesViewModel.Language.ENGLISH -> R.string.options_preferences_language_english
-                    PreferencesViewModel.Language.HUNGARIAN -> R.string.options_preferences_language_hungarian
-                    PreferencesViewModel.Language.ROMANIAN -> R.string.options_preferences_language_romanian
-                }
-            )}"
-        ).apply {
-            setSpan(ForegroundColorSpan(context.obtainColor(android.R.attr.textColorPrimary)), 0, languageText.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-        }
-    )
-
-    private fun updateThemeDescription() = themeLabel.set(
-        SpannableString(
-            "$themeText ${context.getString(
-                when (theme.get()) {
-                    null, PreferencesViewModel.Theme.AUTOMATIC -> R.string.options_preferences_app_theme_automatic
-                    PreferencesViewModel.Theme.DARK -> R.string.options_preferences_app_theme_dark
-                    PreferencesViewModel.Theme.LIGHT -> R.string.options_preferences_app_theme_light
-                }
-            )}"
-        ).apply {
-            setSpan(ForegroundColorSpan(context.obtainColor(android.R.attr.textColorPrimary)), 0, themeText.length, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
-        }
-    )
 }
