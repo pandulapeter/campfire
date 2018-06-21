70a0310b0a13812bf3510f7fcbee00e65d8249ea
Péter Pandula <peter@halcyonmobile.com>
2018-06-21 17:59:04 +0300

    Make sure the What's new snackbar does not appear during the onboarding flow.

--- app/src/main/java/com/pandulapeter/campfire/feature/CampfireActivity.kt
+++ app/src/main/java/com/pandulapeter/campfire/feature/CampfireActivity.kt
@@ -583,13 +583,15 @@ class CampfireActivity : AppCompatActivity(), BaseDialogFragment.OnDialogItemSel
         // Show the changelog snackbar if needed
         if (preferenceDatabase.ftuxLastSeenChangelog < BuildConfig.VERSION_CODE) {
             preferenceDatabase.ftuxLastSeenChangelog = BuildConfig.VERSION_CODE
-            currentFragment?.showSnackbar(
-                message = R.string.options_changelog_app_updated,
-                actionText = R.string.options_changelog_what_is_new,
-                action = {
-                    analyticsManager.onWhatIsNewButtonPressed()
-                    openOptionsScreen(true)
-                })
+            if (preferenceDatabase.isOnboardingDone) {
+                currentFragment?.showSnackbar(
+                    message = R.string.options_changelog_app_updated,
+                    actionText = R.string.options_changelog_what_is_new,
+                    action = {
+                        analyticsManager.onWhatIsNewButtonPressed()
+                        openOptionsScreen(true)
+                    })
+            }
         }
     }
 
