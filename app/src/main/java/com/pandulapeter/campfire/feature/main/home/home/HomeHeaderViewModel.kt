1b5173851e9ab62cea3f2fb88b8ca4ea5b4ca52e
Péter Pandula <peter@halcyonmobile.com>
2018-07-01 12:57:04 +0300

    Animate the shuffle / refresh button.

--- app/src/main/java/com/pandulapeter/campfire/feature/main/home/home/HomeHeaderViewModel.kt
+++ app/src/main/java/com/pandulapeter/campfire/feature/main/home/home/HomeHeaderViewModel.kt
@@ -1,12 +1,16 @@
 package com.pandulapeter.campfire.feature.main.home.home
 
+import android.view.View
+
 data class HomeHeaderViewModel(val title: String, val refreshAction: (() -> Unit)? = null) : HomeItemViewModel {
 
     val shouldShowRefreshButton = refreshAction != null
 
     override fun getItemId() = title.hashCode().toLong()
 
-    fun onRefreshButtonPressed() {
+    fun onRefreshButtonPressed(view: View) {
         refreshAction?.invoke()
+        view.animate().cancel()
+        view.animate().rotationBy(360f).start()
     }
 }
