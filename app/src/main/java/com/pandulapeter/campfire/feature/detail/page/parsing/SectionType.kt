0e73df9ddb1057d99ce16be83a3163785f087a7e
Péter Pandula <peter@halcyonmobile.com>
2018-07-08 11:25:42 +0300

    Add support for pre-chorus section type.

--- app/src/main/java/com/pandulapeter/campfire/feature/detail/page/parsing/SectionType.kt
+++ app/src/main/java/com/pandulapeter/campfire/feature/detail/page/parsing/SectionType.kt
@@ -7,6 +7,7 @@ enum class SectionType(val abbreviation: Char, @StringRes val nameResourceId: In
     INTRO('I', R.string.detail_intro),
     VERSE('V', R.string.detail_verse),
     BRIDGE('B', R.string.detail_bridge),
+    PRE_CHORUS('P', R.string.detail_pre_chorus),
     CHORUS('C', R.string.detail_chorus),
     SOLO('S', R.string.detail_solo),
     OUTRO('O', R.string.detail_outro);
