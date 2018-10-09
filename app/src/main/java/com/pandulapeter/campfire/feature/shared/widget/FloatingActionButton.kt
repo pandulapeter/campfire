19821e2744e31445574d62e6104419a90cf5e883
Péter Pandula <peter@halcyonmobile.com>
2018-10-09 16:48:39 +0300

    Fix FAB visibility issues.

--- app/src/main/java/com/pandulapeter/campfire/feature/shared/widget/FloatingActionButton.kt
+++ app/src/main/java/com/pandulapeter/campfire/feature/shared/widget/FloatingActionButton.kt
@@ -1,5 +1,6 @@
 package com.pandulapeter.campfire.feature.shared.widget
 
+import android.animation.Animator
 import android.content.Context
 import android.support.v7.widget.AppCompatImageView
 import android.util.AttributeSet
@@ -7,6 +8,31 @@ import com.pandulapeter.campfire.R
 import com.pandulapeter.campfire.util.dimension
 
 class FloatingActionButton @JvmOverloads constructor(context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : AppCompatImageView(context, attrs, defStyleAttr) {
+
+    private val hideListener = object : Animator.AnimatorListener {
+
+        override fun onAnimationRepeat(animation: Animator?) = Unit
+
+        override fun onAnimationEnd(animation: Animator?) {
+            visibility = GONE
+        }
+
+        override fun onAnimationCancel(animation: Animator?) = Unit
+
+        override fun onAnimationStart(animation: Animator?) = Unit
+    }
+    private val showListener = object : Animator.AnimatorListener {
+
+        override fun onAnimationRepeat(animation: Animator?) = Unit
+
+        override fun onAnimationEnd(animation: Animator?) = Unit
+
+        override fun onAnimationCancel(animation: Animator?) = Unit
+
+        override fun onAnimationStart(animation: Animator?) {
+            visibility = VISIBLE
+        }
+    }
 
     init {
         setBackgroundResource(R.drawable.bg_floating_action_button)
@@ -19,6 +45,7 @@ class FloatingActionButton @JvmOverloads constructor(context: Context, attrs: At
     }
 
     fun show() {
+        visibility = VISIBLE
         animate().cancel()
         rotation = 0f
         animate().scaleX(1f).scaleY(1f).alpha(1f).apply {
@@ -26,10 +53,12 @@ class FloatingActionButton @JvmOverloads constructor(context: Context, attrs: At
             if (!isVisible()) {
                 rotationBy(360f)
             }
+            setListener(showListener)
         }.start()
     }
 
     fun hide() {
+        visibility = VISIBLE
         animate().cancel()
         rotation = 0f
         animate().scaleX(0f).scaleY(0f).alpha(0f).apply {
@@ -37,6 +66,7 @@ class FloatingActionButton @JvmOverloads constructor(context: Context, attrs: At
             if (isVisible()) {
                 rotationBy(360f)
             }
+            setListener(hideListener)
         }.start()
     }
 
