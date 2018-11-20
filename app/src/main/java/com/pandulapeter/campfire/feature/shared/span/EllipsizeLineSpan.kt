b5be0d643f7d9fd4a618a25ae97c479aef95a28d
Péter Pandula <peter@halcyonmobile.com>
2018-11-20 13:40:02 +0200

    Fix crash caused by incorrect spannable implementation.

--- app/src/main/java/com/pandulapeter/campfire/feature/shared/span/EllipsizeLineSpan.kt
+++ app/src/main/java/com/pandulapeter/campfire/feature/shared/span/EllipsizeLineSpan.kt
@@ -46,17 +46,19 @@ class EllipsizeLineSpan(@ColorInt private val color: Int? = null) : ReplacementS
 
     override fun draw(canvas: Canvas, text: CharSequence, start: Int, end: Int, x: Float, top: Int, y: Int, bottom: Int, paint: Paint) {
         color?.let { paint.color = it }
-        val textWidth = paint.measureText(text, start, end)
-        if (x + ceil(textWidth) < layoutRight) {
-            canvas.drawText(text, start, end, x, y.toFloat(), paint)
-        } else {
-            val ellipsizeWidth = paint.measureText(ELLIPSIZE_CHARACTER)
-            var newEnd = start + paint.breakText(text, start, end, true, layoutRight - x - ellipsizeWidth, null)
-            while (text[newEnd - 1] == ' ' || text[newEnd - 1] == ',' || text[newEnd - 1] == '.') {
-                newEnd--
+        if (start >= 0 && end >= 0 && end <= text.length) {
+            val textWidth = paint.measureText(text, start, end)
+            if (x + ceil(textWidth) < layoutRight) {
+                canvas.drawText(text, start, end, x, y.toFloat(), paint)
+            } else {
+                val ellipsizeWidth = paint.measureText(ELLIPSIZE_CHARACTER)
+                var newEnd = start + paint.breakText(text, start, end, true, layoutRight - x - ellipsizeWidth, null)
+                while (text[newEnd - 1] == ' ' || text[newEnd - 1] == ',' || text[newEnd - 1] == '.') {
+                    newEnd--
+                }
+                canvas.drawText(text, start, newEnd, x, y.toFloat(), paint)
+                canvas.drawText(ELLIPSIZE_CHARACTER, x + paint.measureText(text, start, newEnd), y.toFloat(), paint)
             }
-            canvas.drawText(text, start, newEnd, x, y.toFloat(), paint)
-            canvas.drawText(ELLIPSIZE_CHARACTER, x + paint.measureText(text, start, newEnd), y.toFloat(), paint)
         }
     }
 }
