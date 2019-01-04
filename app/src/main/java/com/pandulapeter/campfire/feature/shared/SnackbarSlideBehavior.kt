21dd8d3d232db71ffe598cce2da846fd5b07383e
Péter Pandula <peter@halcyonmobile.com>
2019-01-04 17:46:39 +0200

    Improve Snackbar behavior on tablets.

--- app/src/main/java/com/pandulapeter/campfire/feature/shared/SnackbarSlideBehavior.kt
+++ app/src/main/java/com/pandulapeter/campfire/feature/shared/SnackbarSlideBehavior.kt
@@ -6,18 +6,23 @@ import android.util.AttributeSet
 import android.view.Gravity
 import android.view.View
 import androidx.coordinatorlayout.widget.CoordinatorLayout
+import com.pandulapeter.campfire.R
 import com.pandulapeter.campfire.util.consume
 
 @Suppress("unused")
-class SnackbarSlideBehavior @JvmOverloads constructor(context: Context? = null, attrs: AttributeSet? = null) : CoordinatorLayout.Behavior<View>(context, attrs) {
+class SnackbarSlideBehavior @JvmOverloads constructor(private val context: Context, attrs: AttributeSet? = null) : CoordinatorLayout.Behavior<View>(context, attrs) {
+
+    private val isPhone get() = !context.resources.getBoolean(R.bool.isTablet)
 
     override fun onAttachedToLayoutParams(lp: CoordinatorLayout.LayoutParams) {
-        if (lp.dodgeInsetEdges == Gravity.NO_GRAVITY) {
-            lp.dodgeInsetEdges = Gravity.BOTTOM
+        if (isPhone) {
+            if (lp.dodgeInsetEdges == Gravity.NO_GRAVITY) {
+                lp.dodgeInsetEdges = Gravity.BOTTOM
+            }
         }
     }
 
-    override fun onLayoutChild(parent: CoordinatorLayout, child: View, layoutDirection: Int) = consume { parent.onLayoutChild(child, layoutDirection) }
+    override fun onLayoutChild(parent: CoordinatorLayout, child: View, layoutDirection: Int) = isPhone && consume { parent.onLayoutChild(child, layoutDirection) }
 
-    override fun getInsetDodgeRect(parent: CoordinatorLayout, child: View, rect: Rect) = consume { rect.set(child.left, child.top, child.right, child.bottom) }
+    override fun getInsetDodgeRect(parent: CoordinatorLayout, child: View, rect: Rect) = isPhone && consume { rect.set(child.left, child.top, child.right, child.bottom) }
 }
