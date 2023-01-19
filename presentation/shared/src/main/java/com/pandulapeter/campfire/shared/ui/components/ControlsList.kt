a5b3a0e23622c341ca95251318504f903dbc6019
Péter Pandula <pandulapeter@gmail.com>
2023-01-19 16:03:10 +0200

    Add button to clear the search query.

--- presentation/shared/src/main/java/com/pandulapeter/campfire/shared/ui/components/ControlsList.kt
+++ presentation/shared/src/main/java/com/pandulapeter/campfire/shared/ui/components/ControlsList.kt
@@ -1,19 +1,28 @@
 package com.pandulapeter.campfire.shared.ui.components
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Checkbox
+import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.unit.dp
 import com.pandulapeter.campfire.data.model.domain.Database
 
@@ -155,6 +164,20 @@ private fun SearchItem(
 ) {
     TextField(
         modifier = Modifier.fillMaxWidth(),
+        trailingIcon = {
+            AnimatedVisibility(
+                modifier = Modifier.fillMaxHeight(),
+                visible = query.isNotEmpty(),
+                enter = fadeIn(),
+                exit = fadeOut()
+            ) {
+                Icon(
+                    modifier = Modifier.wrapContentSize().padding(8.dp).clip(shape = CircleShape).clickable { onQueryChanged("") }.padding(8.dp),
+                    imageVector = Icons.Default.Close,
+                    contentDescription = "Clear",
+                )
+            }
+        },
         label = { Text("Search") },
         value = query,
         onValueChange = onQueryChanged
