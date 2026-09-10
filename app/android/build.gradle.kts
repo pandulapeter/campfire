00d2d219039210ac453420d2920088cb96d99b34
Pandula Péter <pandulapeter@gmail.com>
2026-09-10 17:54:38 +0300

    Move iOS build number to gradle.properties.

--- app/android/build.gradle.kts
+++ app/android/build.gradle.kts
@@ -35,7 +35,7 @@ android {
         applicationId = "com.pandulapeter.campfire"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = targetSdkVersion
-        versionCode = project.property("campfire.versionCode").toString().toInt()
+        versionCode = project.property("campfire.android.versionCode").toString().toInt()
         versionName = project.property("campfire.versionName").toString()
     }
     buildFeatures.compose = true
